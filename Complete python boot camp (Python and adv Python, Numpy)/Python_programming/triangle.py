6c3665b48b450f0064e3352faef4f08f304e9cd1
Krzysztof Tkacz <tkaczk@yahoo.co.uk>
2020-09-30 15:07:47 +0200

    Let's begin

---
+++ Complete python boot camp (Python and adv Python, Numpy)/Python_programming/triangle.py
@@ -0,0 +1,4 @@
+def triangle_area(a, h):
+    return 0.5 * a * h
+area = triangle_area(float(input('Base lenght:\n')),float(input('Height:\n')))
+print("This triangle will have", area, 'square units.')
