1a780643870849aaab7b0dcce8614102a970869a
Krzysztof Tkacz <tkaczk@yahoo.co.uk>
2020-12-18 17:43:03 +0100

    Band name generator ready.

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2021/band_name_generator.py
@@ -0,0 +1,3 @@
+print('Welcome to Band Name Generator')
+band_name = input('What city do You come from?\n') + ' ' + input("What is Your favourite pet's name?\n")
+print("Your band's name will be:\n" + band_name)
